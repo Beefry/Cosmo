01f2e7881e061aea455185e15acf5331adc6dd8f
thecooper <jcoop.del@gmail.com>
2015-08-11 17:40:16 -0400

    Directive Update

--- Cosmo/App/angularjs/Components/Formdisplay/DisplayerDirectives.js
+++ Cosmo/App/angularjs/Components/Formdisplay/DisplayerDirectives.js
@@ -1,71 +1,5 @@
 angular.module('formdisplayer')
-	.directive('formdisplayernew',function(){
-		return {
-			templateUrl:'/Templates/DisplayerNew.htm',
-			restrict:'E',
-			scope: {
-				FormID: "@fid",
-				TemplateID: "@tid"
-			},
-			controllerAs: 'Displayer',
-			controller: ['$scope','formAPI',function($scope,formAPI){
-				var displayer = this;
-
-				console.log($scope.TemplateID);
-
-				if(typeof $scope.FormID != "undefined") {
-					formAPI.get($scope.FormID,function(data) {
-						$scope.model = data;
-					});
-				} else if(typeof $scope.TemplateID != "undefined") {
-					formAPI.newForm($scope.TemplateID,function(data) {
-						$scope.model = data;
-						console.log($scope.model);
-					});
-				}
-
-				$scope.saveForm = function() {
-					formAPI.save($scope.model,function(data){
-						console.log(data);
-					});
-				};
-			}]
-		}
-	})
-	.directive('formdisplayerview',function(){
-		return {
-			templateUrl:'/Templates/DisplayerView.htm',
-			restrict:'E',
-			scope: {
-				FormID: "@fid",
-				TemplateID: "@tid"
-			},
-			controllerAs: 'Displayer',
-			controller: ['$scope','formAPI',function($scope,formAPI){
-				var displayer = this;
-
-				console.log($scope.TemplateID);
-
-				if(typeof $scope.FormID != "undefined") {
-					formAPI.get($scope.FormID,function(data) {
-						$scope.model = data;
-					});
-				} else if(typeof $scope.TemplateID != "undefined") {
-					formAPI.newForm($scope.TemplateID,function(data) {
-						$scope.model = data;
-						console.log($scope.model);
-					});
-				}
-
-				$scope.saveForm = function() {
-					formAPI.save($scope.model,function(data){
-						console.log(data);
-					});
-				};
-			}]
-		}
-	})
-	.directive('formdisplayeredit',function(){
+	.directive('formdisplayer',function(){
 		return {
 			templateUrl:'/Templates/DisplayerEdit.htm',
 			restrict:'E',
@@ -76,8 +10,6 @@ angular.module('formdisplayer')
 			controllerAs: 'Displayer',
 			controller: ['$scope','formAPI',function($scope,formAPI){
 				var displayer = this;
-
-				console.log($scope.TemplateID);
 
 				if(typeof $scope.FormID != "undefined") {
 					formAPI.get($scope.FormID,function(data) {
